8286aee8eca008e2e469d49e7a426828e4f6c2bf
Dirk Uys <dirkcuys@gmail.com>
2013-10-02 14:59:18 -0300

    Fix parameter descriptions and change size to individual width and height parameters

--- bin/s3imageresize.py
+++ bin/s3imageresize.py
@@ -4,10 +4,11 @@ import argparse
 
 from s3imageresize import resize_image_folder
 
-parser = argparse.ArgumentParser(description='Upload a file to Amazon S3 and rotate old backups.')
+parser = argparse.ArgumentParser(description='Resize all images stored in a folder on Amazon S3.')
 parser.add_argument('bucket', help="Name of the Amazon S3 bucket to save the backup file to.")
 parser.add_argument('prefix', help="The prefix to add before the filename for the key.")
-parser.add_argument('psize', help="Path to the file to upload.")
+parser.add_argument('width', help="Maximum width of the image.")
+parser.add_argument('height', help="Maximum height of the image.")
 args = parser.parse_args()
 
-resize_image_folder(args.bucket, args.prefix, args.psize)
+resize_image_folder(args.bucket, args.prefix, (args.width,args.height))
